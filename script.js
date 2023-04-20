7f268e914dd1011307af1781706758b2568218ed
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-04-20 07:45:47 -0300

    layout pagina inicial

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // oi abigos
+axios.defaults.headers.common['Authorization'] = 'aSaefb8T8sX6LpwwvW21qigP';
+
+function listarQuizzes() {
+
+}
+
+function selecionarQuizz() {
+    document.querySelector('.paginaInicial').classList.add('hidden')
+}
