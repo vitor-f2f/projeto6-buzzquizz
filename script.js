952e5dde4ca1884f9ee28c356aeaccb8b786e64e
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-04-20 08:38:36 -0300

    só syncando com outro pc, nada novo pronto

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 // oi abigos
+axios.defaults.headers.common['Authorization'] = 'aSaefb8T8sX6LpwwvW21qigP';
+
+PuxarQuizz();
+
+function PuxarQuizz() {
+    let promise = axios.get(
+      "https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes"
+    );
+    promise.then(ExibirQuizz);
+}
+
+function ExibirQuizz(resposta) {
+    let x = resposta.data;
+    let QuizzesPublicos = document.querySelectorAll(".todosQuizzes.containerQuizzes");
+    QuizzesPublicos.innerHTML = "";
+    for (let index = 0; index < x.length; index++) {
+        QuizzesPublicos.innerHTML += `
+        <div class="caixaQuizz" onclick="selecionarQuizz(${x[index].id})">
+            <img class="thumbnailQuizz" src="${x[index].image}">
+            <span class="tituloQuizz">${x[index].title}</span>
+            <div class="gradientOverlay"></div>
+        </div>
+        `;
+    }
+}
+
 function CriarQuizz(){
     
     const tela1 = document.querySelector('.tela1');
