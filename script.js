a54311bc9f758aeb1431242b48cf22e15adb7f0d
Caio Neme <caiocesarneme@gmail.com>
2023-04-22 02:46:32 -0300

    LocalStorage Funcionando

--- script.js
+++ script.js
@@ -167,8 +167,16 @@ function comparador(){
 let informacaoDoQuizz =[];
 let perguntas = [];
 let nivel = [];
+let quizes = [];
+let quizesStr = "";
+let questoes = [];
 let numeroptg;
 let numeronivel;
+let id = 0;
+let NomeQuizz;
+let ImgQuizz;
+let QtdPerguntasQuizz;
+let QtdNivelQuizz;
 
 function CriarQuizz(){
     
@@ -209,22 +217,39 @@ function AbrirNivel(i){
 
 function CriarPerguntas(){
 
-    const NomeQuizz = document.querySelector('.NomeQuizz').value;
-    const ImgQuizz = document.querySelector('.ImgQuizz').value;
-    const QtdPerguntasQuizz = document.querySelector('.QtdPerguntasQuizz').value;
-    const QtdNivelQuizz = document.querySelector('.QtdNivelQuizz').value;
+    
+    NomeQuizz = document.querySelector('.NomeQuizz').value;
+    ImgQuizz = document.querySelector('.ImgQuizz').value;
+    QtdPerguntasQuizz = document.querySelector('.QtdPerguntasQuizz').value;
+    QtdNivelQuizz = document.querySelector('.QtdNivelQuizz').value;
 
     numeroptg = QtdPerguntasQuizz;
     numeronivel = QtdNivelQuizz;
 
     if(NomeQuizz.length >= 20 && NomeQuizz.length <=65 && QtdPerguntasQuizz >=3 && QtdNivelQuizz>=2){
 
-        informacaoDoQuizz = [{nome: NomeQuizz , imagem: ImgQuizz, perguntas:QtdPerguntasQuizz, nivel:QtdNivelQuizz}];
+        informacaoDoQuizz = [{nome: NomeQuizz, 
+                              imagem: ImgQuizz, 
+                              perguntas:QtdPerguntasQuizz, 
+                              nivel:QtdNivelQuizz}];
 
-        const pag3_1 = document.querySelector('.pag3_1');
-        const pag3_2 = document.querySelector('.pag3_2');
-        const pag3_3 = document.querySelector('.pag3_3');
-        const pag3_4 = document.querySelector('.pag3_4');
+        
+        let pag3_1 = document.querySelector('.pag3_1');
+        let pag3_2 = document.querySelector('.pag3_2');
+        let pag3_3 = document.querySelector('.pag3_3');
+        let pag3_4 = document.querySelector('.pag3_4');
+        
+        pag3_2.innerHTML = "";
+        pag3_3.innerHTML = "";
+        pag3_4.innerHTML = "";
+        pag3_1 = document.querySelector('.pag3_1');
+        pag3_2 = document.querySelector('.pag3_2');
+        pag3_3 = document.querySelector('.pag3_3');
+        pag3_4 = document.querySelector('.pag3_4');
+        
+        pag3_2.innerHTML += `<h1 class="titulo margintop">Crie suas perguntas</h1>`;
+        pag3_3.innerHTML += `<h1 class="titulo margintop">Agora, decida os níveis! </h1>`;
+        pag3_4.innerHTML += `<h1 class="titulo margintop">Seu quizz está pronto!</h1>`
 
         for(let i = 0; i < QtdPerguntasQuizz; i++){
            if(i === 0){
@@ -236,26 +261,26 @@ function CriarPerguntas(){
                                 </div>
                                 <div class="containerInputs pergunta">
                                     <div class="inputs">
-                                        <input type="text" class="input pergunta${i}" minlength="20" placeholder="Texto da pergunta">
-                                        <input type="text" value="#FFFFFF" class="input cor${i}" placeholder="Cor de fundo da pergunta">
+                                        <input type="text" value="" class="input pergunta${i}" minlength="20" placeholder="Texto da pergunta">
+                                        <input type="text" value="#9400D3" class="input cor${i}" placeholder="Cor de fundo da pergunta">
                                     </div>
                                     <div class="inputs">
                                         <h1 class="titulo">Resposta correta</h1>
-                                        <input type="text" class="input respostaCorreta${i}" minlength="1" placeholder="Resposta correta">
-                                        <input type="url" class="input URLrespostaCorreta${i}" placeholder="URL da imagem">
+                                        <input type="text" value="" class="input respostaCorreta${i}" minlength="1" placeholder="Resposta correta">
+                                        <input type="url" value="" class="input URLrespostaCorreta${i}" placeholder="URL da imagem">
                                     </div>
                                     <div class="inputs">
                                         <h1 class="titulo">Respostas incorretas</h1>
-                                        <input type="text" class="input respostaIncorreta${i}_1" placeholder="Resposta incorreta 1">
-                                        <input type="url" class="input URLrespostaIncorreta${i}_1" placeholder="URL da imagem 1">
+                                        <input type="text" value="" class="input respostaIncorreta${i}_1" placeholder="Resposta incorreta 1">
+                                        <input type="url" value="" class="input URLrespostaIncorreta${i}_1" placeholder="URL da imagem 1">
                                     </div>
                                     <div class="inputs">
-                                        <input type="text" class="input respostaIncorreta${i}_2" placeholder="Resposta incorreta 2">
-                                        <input type="url" class="input URLrespostaIncorreta${i}_2" placeholder="URL da imagem 2">
+                                        <input type="text" value="" class="input respostaIncorreta${i}_2" placeholder="Resposta incorreta 2">
+                                        <input type="url" value="" class="input URLrespostaIncorreta${i}_2" placeholder="URL da imagem 2">
                                     </div>
                                     <div class="inputs">
-                                        <input type="text" class="input respostaIncorreta${i}_3" placeholder="Resposta incorreta 3">
-                                        <input type="url" class="input URLrespostaIncorreta${i}_3" placeholder="URL da imagem 3">
+                                        <input type="text" value="" class="input respostaIncorreta${i}_3" placeholder="Resposta incorreta 3">
+                                        <input type="url" value="" class="input URLrespostaIncorreta${i}_3" placeholder="URL da imagem 3">
                                     </div>
                                 </div>
                                 `;
@@ -269,26 +294,26 @@ function CriarPerguntas(){
                                 </div>
                                 <div class="containerInputs pergunta escondido">
                                     <div class="inputs">
-                                        <input type="text" class="input pergunta${i}" minlength="20" placeholder="Texto da pergunta">
-                                        <input type="text" value="#FFFFFF" class="input cor${i}" placeholder="Cor de fundo da pergunta">
+                                        <input type="text" value="" class="input pergunta${i}" minlength="20" placeholder="Texto da pergunta">
+                                        <input type="text" value="#9400D3" class="input cor${i}" placeholder="Cor de fundo da pergunta">
                                     </div>
                                     <div class="inputs">
                                         <h1 class="titulo">Resposta correta</h1>
-                                        <input type="text" class="input respostaCorreta${i}" minlength="1" placeholder="Resposta correta">
-                                        <input type="url" class="input URLrespostaCorreta${i}" placeholder="URL da imagem">
+                                        <input type="text" value="" class="input respostaCorreta${i}" minlength="1" placeholder="Resposta correta">
+                                        <input type="url" value="" class="input URLrespostaCorreta${i}" placeholder="URL da imagem">
                                     </div>
                                     <div class="inputs">
                                         <h1 class="titulo">Respostas incorretas</h1>
-                                        <input type="text" class="input respostaIncorreta${i}_1" placeholder="Resposta incorreta 1">
-                                        <input type="url" class="input URLrespostaIncorreta${i}_1" placeholder="URL da imagem 1">
+                                        <input type="text" value="" class="input respostaIncorreta${i}_1" placeholder="Resposta incorreta 1">
+                                        <input type="url" value="" class="input URLrespostaIncorreta${i}_1" placeholder="URL da imagem 1">
                                     </div>
                                     <div class="inputs">
-                                        <input type="text" class="input respostaIncorreta${i}_2" placeholder="Resposta incorreta 2">
-                                        <input type="url" class="input URLrespostaIncorreta${i}_2" placeholder="URL da imagem 2">
+                                        <input type="text" value="" class="input respostaIncorreta${i}_2" placeholder="Resposta incorreta 2">
+                                        <input type="url" value="" class="input URLrespostaIncorreta${i}_2" placeholder="URL da imagem 2">
                                     </div>
                                     <div class="inputs">
-                                        <input type="text" class="input respostaIncorreta${i}_3" placeholder="Resposta incorreta 3">
-                                        <input type="url" class="input URLrespostaIncorreta${i}_3" placeholder="URL da imagem 3">
+                                        <input type="text" value="" class="input respostaIncorreta${i}_3" placeholder="Resposta incorreta 3">
+                                        <input type="url" value="" class="input URLrespostaIncorreta${i}_3" placeholder="URL da imagem 3">
                                     </div>
                                 </div>
                                 `;
@@ -303,10 +328,10 @@ function CriarPerguntas(){
                                     <ion-icon name="create-outline"></ion-icon>
                                 </div>
                                 <div class="inputs nivel">                                   
-                                    <input type="text" class="input tituloNivel${i}" minlength="10" placeholder="Título do nível">
-                                    <input type="number" class="input porcentagemNivel${i}" min="0" max="100" placeholder="% de acerto mínima">
-                                    <input type="url" class="input imagemNivel${i}" placeholder="URL da imagem do nível">
-                                    <input type="text" class="input descricaoNivel${i}" minlength="30" placeholder="Descrição do nível">
+                                    <input value="" type="text" class="input tituloNivel${i}" minlength="10" placeholder="Título do nível">
+                                    <input value="" type="number" class="input porcentagemNivel${i}" min="0" max="100" placeholder="% de acerto mínima">
+                                    <input value="" type="url" class="input imagemNivel${i}" placeholder="URL da imagem do nível">
+                                    <input value="" type="text" class="input descricaoNivel${i}" minlength="30" placeholder="Descrição do nível">
                                 </div>
                                 `;
             }
@@ -318,10 +343,10 @@ function CriarPerguntas(){
                                     <ion-icon name="create-outline"></ion-icon>
                                 </div>
                                 <div class="inputs nivel escondido">                                   
-                                    <input type="text" class="input tituloNivel${i}" minlength="10" placeholder="Título do nível">
-                                    <input type="number" class="input porcentagemNivel${i}" min="0" max="100" placeholder="% de acerto mínima">
-                                    <input type="url" class="input imagemNivel${i}" placeholder="URL da imagem do nível">
-                                    <input type="text" class="input descricaoNivel${i}" minlength="30" placeholder="Descrição do nível">
+                                    <input value="" type="text" class="input tituloNivel${i}" minlength="10" placeholder="Título do nível">
+                                    <input value="" type="number" class="input porcentagemNivel${i}" min="0" max="100" placeholder="% de acerto mínima">
+                                    <input value="" type="url" class="input imagemNivel${i}" placeholder="URL da imagem do nível">
+                                    <input value="" type="text" class="input descricaoNivel${i}" minlength="30" placeholder="Descrição do nível">
                                 </div>
                                 `;
             }
@@ -339,6 +364,8 @@ function CriarPerguntas(){
                         <div class="BotaoVermelho Tela3_AcessarQuizz" onclick="jogarQuizz()">Acessar Quizz</div>
                         <div class="Texto_Cinza Tela3_VoltarHome" onclick="voltarHome()">Voltar pra home</div>
                         `;
+
+
         pag3_1.classList.add('escondido');
         pag3_2.classList.remove('escondido');
 
@@ -349,7 +376,7 @@ function CriarPerguntas(){
 }
 
 function CriarNives(){
-        
+    const hex =['a','b','c','d','e','f','A','B','C','D','E','F','0','9','8','7','6','5','4','3','2','1']
     let pergunta = document.querySelector(`.pergunta0`).value;
     let cor = document.querySelector(`.cor0`).value;
     let respostaCorreta = document.querySelector(`.respostaCorreta0`).value;
@@ -361,13 +388,13 @@ function CriarNives(){
     let respostaIncorreta_3 = document.querySelector(`.respostaIncorreta0_3`).value;
     let URLrespostaIncorreta_3 = document.querySelector(`.URLrespostaIncorreta0_3`).value;
     
-        if(pergunta.length >= 20 && cor[0]=='#' && respostaCorreta !='' && respostaIncorreta_1 != ''){
+        if(pergunta.length >= 20 && cor[0]=='#' && respostaCorreta !='' && respostaIncorreta_1 != '' && hex.includes(cor[1]) > 0 && hex.includes(cor[2]) > 0 && hex.includes(cor[3]) > 0 && hex.includes(cor[4]) > 0 && hex.includes(cor[5]) > 0 && hex.includes(cor[6]) > 0){
             for(let c =0; c < numeroptg; c++){
                 pergunta = document.querySelector(`.pergunta${c}`).value;
                 cor = document.querySelector(`.cor${c}`).value;
                 respostaCorreta = document.querySelector(`.respostaCorreta${c}`).value;
                 URLrespostaCorreta = document.querySelector(`.URLrespostaCorreta${c}`).value;
-                respostaIncorreta_1 = document.querySelector(`.respostaIncorreta${c}_1`).value
+                respostaIncorreta_1 = document.querySelector(`.respostaIncorreta${c}_1`).value;
                 URLrespostaIncorreta_1 = document.querySelector(`.URLrespostaIncorreta${c}_1`).value;
                 respostaIncorreta_2 = document.querySelector(`.respostaIncorreta${c}_2`).value;
                 URLrespostaIncorreta_2 = document.querySelector(`.URLrespostaIncorreta${c}_2`).value;
@@ -386,10 +413,51 @@ function CriarNives(){
                     respostaIncorreta_3: respostaIncorreta_3, 
                     URLrespostaIncorreta_3: URLrespostaIncorreta_3
                 }];
-                perguntas.push(x);
+                perguntas.push(x);  
+            
+                let resposta0 = {
+                                text:respostaCorreta,
+                                image:URLrespostaCorreta,
+                                isCorrectAnswer:true
+                                };
+
+                let resposta1 = {
+                                text:respostaIncorreta_1,
+                                image:URLrespostaIncorreta_1,
+                                isCorrectAnswer:false
+                               };
                 
+                let resposta2 = {
+                                text:respostaIncorreta_2,
+                                image:URLrespostaIncorreta_2,
+                                isCorrectAnswer:false
+                               };
+
+                let resposta3 = {
+                                text:respostaIncorreta_3,
+                                image:URLrespostaIncorreta_3,
+                                isCorrectAnswer:false
+                               };
+
+                let resposta = [];
+
+                resposta.push(resposta0);
+                resposta.push(resposta1);
+
+                if(resposta2.text != ""){
+                    resposta.push(resposta2);
+                }
+                if(resposta3.text != ""){
+                    resposta.push(resposta3);
+                }
+                let toSurtando ={
+                                answers:resposta,
+                                title:pergunta,
+                                color:cor
+                                };
+                questoes.push(toSurtando);
             }
-            console.log(perguntas);
+            
             const pag3_2 = document.querySelector('.pag3_2');
                 pag3_2.classList.add('escondido');
                 
@@ -398,42 +466,58 @@ function CriarNives(){
         }
         else{
             alert('Confira os dados');
-            console.log(perguntas);
         }
     
 }
 
 function AddQuizz(){
+    
     for(let c = 0; c < numeronivel; c++){
         let tituloNivel = document.querySelector(`.tituloNivel${c}`).value;
-        let porcentagemNivel = document.querySelector(`.porcentagemNivel${c}`).value;
+        let porcentagemNivel =document.querySelector(`.porcentagemNivel${c}`).value;
         let imagemNivel = document.querySelector(`.imagemNivel${c}`).value;
         let descricaoNivel = document.querySelector(`.descricaoNivel${c}`).value;
         
-        if(tituloNivel.length >= 10 && descricaoNivel.length >= 30){
-            let x =[{
-                    tituloNivel: tituloNivel,
-                    porcentagemNivel: porcentagemNivel,
-                    imagemNivel: imagemNivel,
-                    descricaoNivel:descricaoNivel
-                    }]
+        if(tituloNivel.length >= 10 && descricaoNivel.length >= 30 && porcentagemNivel > -1 && porcentagemNivel <101){
+            let x ={
+                    title: tituloNivel,
+                    minValue: porcentagemNivel,
+                    image: imagemNivel,
+                    text:descricaoNivel
+                    };
             nivel.push(x);
+            
         }
         else{
             alert('Confia os dados');
             return;
         }
     }
+    id++
+    let z = {
+        id: id,
+        image:informacaoDoQuizz[0].imagem,
+        levels:nivel,
+        questions:questoes,
+        title:informacaoDoQuizz[0].nome
+    };
 
-        const pag3_3 = document.querySelector('.pag3_3');
-        pag3_3.classList.add('escondido');
+    quizes.push(z);
+    console.log(quizes);
 
-        const pag3_4 = document.querySelector('.pag3_4');
-        pag3_4.classList.remove('escondido');
+    quizesStr = JSON.stringify(quizes);
+    localStorage.setItem("id", quizesStr)
+    const pag3_3 = document.querySelector('.pag3_3');
+    pag3_3.classList.add('escondido');
+
+    const pag3_4 = document.querySelector('.pag3_4');
+    pag3_4.classList.remove('escondido');
 
 }
 
 function voltarHome(){
+    checkLocalStorage(quizesStr);
+
     const tela1 = document.querySelector('.tela1');
     tela1.classList.remove('escondido');
 
